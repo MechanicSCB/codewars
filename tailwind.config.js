b7869002b9655730cdbd3abfc9a911406968adfe
Nikolay <kolek-ulyanov@ya.ru>
2022-12-30 15:13:49 +0300

    ref_2

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,50 @@ module.exports = {
         './resources/views/**/*.blade.php',
         './resources/js/**/*.vue',
     ],
-
+    darkMode: 'class',
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                ui: {
+                    // body: 'rgb(var(--color-ui-bg) / <alpha-value>)',
+                    // section: 'rgb(var(--color-ui-section) / <alpha-value>)',
+                    // accent: 'rgb(var(--color-ui-accent) / <alpha-value>)',
+                    body: 'var(--color-ui-bg)',
+                    section: 'var(--color-ui-section)',
+                    accent: 'var(--color-ui-accent)',
+                    text: 'var(--color-ui-text)',
+                    link: {
+                        text: 'var(--color-ui-link-text)',
+                        hover: 'var(--color-ui-link-text-hover)',
+                        active: 'var(--color-ui-link-text-active)',
+                        disabled: 'var(--color-ui-link-text-disabled)',
+                        alt: {
+                            text: 'var(--color-ui-alt-link-text)',
+                            hover: 'var(--color-ui-alt-link-text-hover)',
+                            active: 'var(--color-ui-alt-link-text-active)',
+                        },
+
+                    },
+                },
+                gray: {
+                    '50': '#fafafa',
+                    '100': '#f4f4f5',
+                    '200': '#e4e4e7',
+                    '300': '#d4d4d8',
+                    '400': '#a1a1aa',
+                    '500': '#71717a',
+                    '600': '#52525b',
+                    '700': '#3f3f46',
+                    '800': '#27272a',
+                    '900': '#18181b',
+                },
+
+            },
+            fontSize: {
+                'xxs': '.6rem',
             },
         },
     },
