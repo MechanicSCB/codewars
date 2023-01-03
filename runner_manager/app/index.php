41db6a211dd3767d1e6050c23ef52ecbaf373b13
Nikolay <kolek-ulyanov@ya.ru>
2023-01-03 15:55:12 +0300

    runner_manager ref_3

--- runner_manager/app/index.php
+++ runner_manager/app/index.php
@@ -1,13 +1,8 @@
 <?php
 
-$cmd = 'docker -v';
-//$cmd = 'docker run -dp 8081:80 phpmyadmin 2>&1';
 //$cmd = 'docker run -dp 5099:80 -e APACHE_RUN_USER=#1000  kolekulyanov/apache-runner 2>&1';
-$cmd = 'docker run -dp 81:80 kolekulyanov/sail-runner:2.1 2>&1';
-//$cmd = 'docker stop ee889904eec29e418a49d299f42c57e6130768b5d4e35b4b445f4e24c4b33d1d';
-//$cmd = 'docker rm ee889904eec29e418a49d299f42c57e6130768b5d4e35b4b445f4e24c4b33d1d';
-//$cmd = 'docker ps -a';
-//$cmd = 'whoami';
+//$cmd = 'docker run -dp 81:80 --name laravel_runner kolekulyanov/sail-runner:2.1 2>&1';
+$cmd = 'docker restart codewars-laravel-runner-1 codewars-apache-runner-1 2>&1';
 
 $msg = shell_exec($cmd);
 
